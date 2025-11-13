332f7d11bc5e470479ffa8defd9387e065a200c8
pblyat <agag5@naver.com>
2025-11-14 00:47:14 +0900

    Update checkserver.py

--- checkserver.py
+++ checkserver.py
@@ -24,6 +24,7 @@ def getdata():
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
             'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+            'X-Timezone': 'Asia/Seoul'
         }
         response = requests.get(mmhome, headers=headers, timeout=30)
         response.raise_for_status()
